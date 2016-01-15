786f5dc871772e160ad1e5d230e46147134f2c94
Yarik <yaroslaff.k@gmail.com>
2016-01-15 21:34:48 +0200

    industrial java quizes added

---
+++ src/industrialjava/chapter1/Quest5.java
@@ -0,0 +1,9 @@
+package industrialjava.chapter1;
+
+public class Quest5{
+    public static void main(){
+        System.out.print("А");  }
+    public static void main(String args){
+        System.out.print("Б");  }
+    public static void main(String[] args){
+        System.out.print("В");  } }
---
+++ src/industrialjava/chapter1/Task3.java
@@ -0,0 +1,8 @@
+package industrialjava.chapter1;
+
+class Test3 {
+    public static void main(String s[ ]) {
+        String args;
+        /*System.out.print(args + s);*/
+    }
+}
